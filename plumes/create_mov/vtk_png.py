fdce7798a9ae7c8549dae4ed6b2e4d1b62546dc1
shubham-688 <agr.shubh.agr@gmail.com>
2025-08-02 19:13:49 -0400

    changed size

--- plumes/create_mov/vtk_png.py
+++ plumes/create_mov/vtk_png.py
@@ -5,8 +5,8 @@ import os
 import sys
 
 # Filename stuff
-vtk_dir = "../output_10sec/elements/orthogonal_azimuthal_slices/vtk/temp0/"
-output_dir = "../output_10sec/elements/orthogonal_azimuthal_slices/vtk_pngs/temp0/"
+vtk_dir = "../output_10sec/elements/orthogonal_azimuthal_slices/vtk/slice3/"
+output_dir = "../output_10sec/elements/orthogonal_azimuthal_slices/vtk_pngs/slice3/"
 os.makedirs(output_dir, exist_ok=True)
 # path_to_vtk  = '/Users/eaton/Downloads/'
 wave_id      = 100
@@ -16,8 +16,8 @@ slicename = f'wave{wave_id}'
 vtk_files = sorted([f for f in os.listdir(vtk_dir) if f.endswith('.vtk')])
 for i, vtk_file in enumerate(vtk_files):
 # for vtk_file in vtk_files:
-    outpath = os.path.join(output_dir, f"frame_{i:04d}.png")
-    wave100vtk = LegacyVTKReader(FileNames=[os.path.join(vtk_dir, vtk_files[0])])
+    outpath = os.path.join(output_dir, f"{vtk_file[:-3]}png")
+    wave100vtk = LegacyVTKReader(FileNames=[os.path.join(vtk_dir, vtk_file)])
     # wave100vtk = LegacyVTKReader(registrationName=f'{slicename}.vtk',
     #                              FileNames=[f'{path_to_vtk}/{slicename}.vtk'])
 
@@ -29,8 +29,10 @@ for i, vtk_file in enumerate(vtk_files):
     wave100vtkDisplay = Show(wave100vtk, rv, 'UnstructuredGridRepresentation')
 
     # Adjust the camera to be orthogonal to slice
-    rv.ResetActiveCameraToPositiveY()
-    rv.ResetCamera(False, 0.9)
+    # rv.ResetActiveCameraToPositiveY()# slice0
+    rv.ResetActiveCameraToPositiveX()# slice0
+
+    rv.ResetCamera(False, 0.9) # slice0
     wave100vtkDisplay = Show(wave100vtk, rv, 'UnstructuredGridRepresentation')
 
     # Change colourbar limits
@@ -43,7 +45,7 @@ for i, vtk_file in enumerate(vtk_files):
     # Save with transparent background
     SaveScreenshot(filename=outpath,
                    viewOrLayout=rv,
-                   ImageResolution=[2100, 1086],
+                   ImageResolution=[1050, 543],#[2100, 1086]
                    TransparentBackground=1)
     print("Saved to ", outpath)
     # sys.exit()
